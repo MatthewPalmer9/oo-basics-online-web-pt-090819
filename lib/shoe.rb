a2d65d2b5f0918af530975951694d5cb4cfbe606
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-24 23:42:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,23 @@
 class Shoe 
+  attr_accessor :brand, :color, :size, :material, :condition
   
+  def initialize(brand)
+    @brand
+  end 
+  
+  def color 
+    @color 
+  end 
+  
+  def size
+    @size 
+  end 
+  
+  def material
+    @material
+  end 
+  
+  def condition
+    @condition
+  end 
 end 
